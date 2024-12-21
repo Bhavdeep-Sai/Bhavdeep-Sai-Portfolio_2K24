10937d966af6fe477a53bcd656d7b8afa48fba79
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2024-12-21 11:37:56 +0530

    change in header and added responsiveness

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
+  });
+});
+
 // Typewriter Effect
 const texts = [
     "WEB-DEVELOPER",
     "DESIGNER",
-    "Editor"
+    "Video-Editor"
 ]
 
 let speed  =100;
